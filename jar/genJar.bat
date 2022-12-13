297579fbe9b3b99cb844903f0746f1a0ea317841
caoqiang <Xinyan1203>
2022-12-13 22:26:37 +0800

    up

--- jar/genJar.bat
+++ jar/genJar.bat
@@ -5,7 +5,7 @@ del "%~dp0\custom_spider.jar"
 rd /s/q "%~dp0\Smali_classes"
 
 dir /b /s "%~dp0\..\app\build\intermediates\dex
-if exist "%~dp0\..\app\build\intermediates\dex\release\minifyReleaseWithR8 (
+if exist "%~dp0\..\app\build\intermediates\dex\release\minifyReleaseWithR8" (
     java -jar "%~dp0\3rd\baksmali-2.5.2.jar" d "%~dp0\..\app\build\intermediates\dex\release\minifyReleaseWithR8\classes.dex" -o "%~dp0\Smali_classes"
 ) else (
     java -jar "%~dp0\3rd\baksmali-2.5.2.jar" d "%~dp0\..\app\build\intermediates\dex\release\mergeDexRelease\classes.dex" -o "%~dp0\Smali_classes"
